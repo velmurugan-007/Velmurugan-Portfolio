15bab4d04787e0a6a080652a98375ec12f5a6994
velmurugan-007 <vel.git.007@gmail.com>
2025-05-26 11:16:48 +0530

    Add Bootstrap and fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+// Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
